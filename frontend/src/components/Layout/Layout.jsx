8b7318d53c9da18b8a4e17576c4ab71783f40218
shevkenov <shevkenov@gmail.com>
2020-06-23 11:12:13 +0300

    add some ligic to importCountedArticles

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,115 +1,115 @@
 import React, { Fragment, Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import axios from '../../axios-connection';
-import XLSX from 'xlsx';
-import FileSaver from 'file-saver';
+import axios from "../../axios-connection";
+import XLSX from "xlsx";
+import FileSaver from "file-saver";
 
 import Toolbar from "../Toolbar/Toolbar";
 import Inventory from "../../containers/Inventory/Inventory";
 import importFiles from "../../assets/importFiles";
 import Home from "../Home/Home";
-import convertXLSXtoCSV from '../../assets/convertXLStoCSV';
+import convertXLSXtoCSV from "../../assets/convertXLStoCSV";
 
 class Layout extends Component {
   state = {
-    articles: [ 
-    //   {
-    //     sapcode: "123456789",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456788",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456787",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456786",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456785",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456784",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456783",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла1",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456782",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла2",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456781",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла3",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
-    //   {
-    //     sapcode: "123456780",
-    //     barcode: ["1234567890123"],
-    //     inStock: 1,
-    //     orpakStock: 3,
-    //     group: "Масла",
-    //     name: "Proba",
-    //     supplier: "Supplier",
-    //   },
+    articles: [
+      //   {
+      //     sapcode: "123456789",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456788",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456787",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456786",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456785",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456784",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456783",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла1",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456782",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла2",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456781",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла3",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
+      //   {
+      //     sapcode: "123456780",
+      //     barcode: ["1234567890123"],
+      //     inStock: 1,
+      //     orpakStock: 3,
+      //     group: "Масла",
+      //     name: "Proba",
+      //     supplier: "Supplier",
+      //   },
     ],
     error: "",
     loading: false,
     articlesImported: false,
     ascending: true,
     gasStations: {},
-    searchedValue: {}
+    searchedValue: {},
   };
 
   componentDidMount() {
@@ -133,8 +133,8 @@ class Layout extends Component {
       sapXLSX: "sap.xlsx",
       sapXLS: "sap.xls",
       orpakXLSX: "orpak.xlsx",
-      orpakXLS: "orpak.xls"
-    }
+      orpakXLS: "orpak.xls",
+    };
 
     const filesArr = [];
     for (let index = 0; index < files.length; index++) {
@@ -146,9 +146,11 @@ class Layout extends Component {
       filesArr.find((f) => f.name.toLowerCase() === allowedFileNames.sapXLSX);
 
     const orpakFile =
-      filesArr.find(f => f.name.toLowerCase() === allowedFileNames.orpakXLS) ||
+      filesArr.find(
+        (f) => f.name.toLowerCase() === allowedFileNames.orpakXLS
+      ) ||
       filesArr.find((f) => f.name.toLowerCase() === allowedFileNames.orpakXLSX);
-    
+
     const fuelStationFile = filesArr.find((f) => {
       const arr = f.name.split(".");
       const name = arr[0];
@@ -189,17 +191,17 @@ class Layout extends Component {
       const fsData = await fsBlob.text();
       const orpakData = await convertXLSXtoCSV(orpakFile);
       const sapData = await convertXLSXtoCSV(sapFile);
-      
+
       //const file = new FileReader();
       //file.readAsText(sapFile, "windows-1251");
 
       //file.onload = () => {
-        this.setState({
-          //articles: importFiles(file.result, orpakData, fsData),
-          articles: importFiles(sapData, orpakData, fsData),
-          loading: false,
-          articlesImported: true,
-        });
+      this.setState({
+        //articles: importFiles(file.result, orpakData, fsData),
+        articles: importFiles(sapData, orpakData, fsData),
+        loading: false,
+        articlesImported: true,
+      });
       //};
     } catch (error) {
       this.setState({
@@ -302,25 +304,23 @@ class Layout extends Component {
   };
 
   exportOrpak = () => {
-    const articles = this.state.articles.reduce((acc,item) => {
+    const articles = this.state.articles.reduce((acc, item) => {
       const qty = item.inStock;
-      if(qty > 0) {
-        const intQty = ("0000000000" + parseInt(qty)).slice(-10);
+      const intQty = ("0000000000" + parseInt(qty)).slice(-10);
 
-        const inx = qty.toString().indexOf(".");
-        let decimalQty = "000";
-        if (inx > 0) {
-          decimalQty = qty.toString().substring(inx + 1);
-          decimalQty = ("000" + decimalQty).slice(-3);
-        }
-        const brc = item.barcode;
-        const barcode =
-          brc === "-" ? "0000000000000" : ("000000000000" + brc).slice(-13);
-        acc.push(`${barcode}${intQty}${decimalQty}`);
+      const inx = qty.toString().indexOf(".");
+      let decimalQty = "000";
+      if (inx > 0) {
+        decimalQty = qty.toString().substring(inx + 1);
+        decimalQty = ("000" + decimalQty).slice(-3);
       }
+      const brc = item.barcode;
+      const barcode =
+        brc === "-" ? "0000000000000" : ("000000000000" + brc).slice(-13);
+      acc.push(`${barcode}${intQty}${decimalQty}`);
 
       return acc;
-    },[]);
+    }, []);
 
     let csvContent = "data:text/csv;charset=utf-8," + articles.join("\n");
 
@@ -330,7 +330,7 @@ class Layout extends Component {
     link.setAttribute("download", "OrpakInventory.txt");
     document.body.appendChild(link); // Required for FF
 
-    link.click(); // This will download the data file named "my_data.csv".
+    link.click();
   };
 
   exportXLSX = () => {
@@ -372,66 +372,62 @@ class Layout extends Component {
   searchHandler = (value) => {
     const searchedValue = {
       value: value.inputValue,
-      name: value.value
-    }
-    this.setState(
-      {searchedValue}
-    )
-    
+      name: value.value,
+    };
+    this.setState({ searchedValue });
   };
 
   showArticles = () => {
-    if (this.state.searchedValue.value){
+    if (this.state.searchedValue.value) {
       const val = this.state.searchedValue.value.toLowerCase();
       const regex = new RegExp(`[\\d\\w]*${val}[\\d\\w]*`);
-      const articles = this.state.articles.reduce((acc,cur) => {
+      const articles = this.state.articles.reduce((acc, cur) => {
         const item = cur[this.state.searchedValue.name].toLowerCase();
-        if(regex.test(item)){
-          acc.push(cur)
-        }
-        
-        return acc
-      },[]);
-      return articles;
-    }else{
-      return this.state.articles
-    }
-    
-  }
-
-  inventoryImportHandler = async(event) => {
-    const file = event.target.files[0];
-    this.setState({
-      articles: [],
-      loading: true
-    });
-    try {
-
-      const data = await convertXLSXtoCSV(file);
-      const articles = data.split("\n").slice(1).reduce((acc,cur) => {
-        const item = cur.split(";");
-        if(item.length === 7){
-          acc.push({
-            sapcode: item[0].trim(),
-            barcode: [item[1].trim()],
-            name: item[2].trim(),
-            group: item[3].trim(),
-            supplier: item[4].trim(),
-            orpakStock: item[5].replace(",", "."),
-            inStock: item[6].replace(",", "."),
-          });
+        if (regex.test(item)) {
+          acc.push(cur);
         }
 
         return acc;
-      },[]);
+      }, []);
+      return articles;
+    } else {
+      return this.state.articles;
+    }
+  };
 
-      this.setState(
-        {
-          articles,
-          loading: false,
-          articlesImported: true,
-        }
-      );
+  inventoryImportHandler = async (event) => {
+    const file = event.target.files[0];
+    this.setState({
+      articles: [],
+      loading: true,
+    });
+    try {
+      const data = await convertXLSXtoCSV(file);
+      const articles = data
+        .split("\n")
+        .slice(1)
+        .reduce((acc, cur) => {
+          const item = cur.split(";");
+          if (item.length === 7) {
+            acc.push({
+              sapcode: item[0].trim(),
+              barcode: [item[1].trim()],
+              name: item[2].trim(),
+              group: item[3].trim(),
+              supplier: item[4].trim(),
+              orpakStock: item[5].replace(",", "."),
+              inStock: item[6].replace(",", "."),
+            });
+          }
+
+          return acc;
+        }, []);
+
+      this.setState({
+        articles,
+        loading: false,
+        articlesImported: true,
+      });
     } catch (error) {
       this.setState({
         loading: false,
