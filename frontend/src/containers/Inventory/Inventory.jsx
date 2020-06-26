20abcf643d3bb5bc0a63aae14aea72ebc9177535
shevkenov <shevkenov@gmail.com>
2020-06-26 10:30:41 +0300

    show only items with orpakStock and instock quantities

--- frontend/src/containers/Inventory/Inventory.jsx
+++ frontend/src/containers/Inventory/Inventory.jsx
@@ -34,37 +34,42 @@ class Inventory extends Component {
 
   render() {
     let index = 0;
-    const articles = this.props.articles().map((item) => {
-      //const barcode = item.barcode ? item.barcode : [item.sapcode];
+    const articles = this.props.articles().reduce((acc, item) => {
       let difference = item.inStock - item.orpakStock;
       difference =
         (difference + "").indexOf(".") === -1
           ? difference
           : difference.toFixed(2);
-      return (
-        <tr key={index}>
-          <td className={classes.No}>{++index}</td>
-          <td>{item.sapcode}</td>
-          <td>{item.barcode}</td>
-          <td>{item.name}</td>
-          <td>{item.group}</td>
-          <td>{item.supplier}</td>
-          <td className={classes.Number}>{item.orpakStock}</td>
-          <td className={classes.Input}>
-            <div>
-              <input
-                value={item.inStock}
-                type="number"
-                min="0"
-                onChange={(e) => this.props.onChange(e, item.sapcode)}
-              />
-            </div>
-          </td>
-          <td className={classes.Number}>{difference}</td>
-          <Button onClick={() => this.popupMessage(item)}>Изтрий</Button>
-        </tr>
-      );
-    });
+      
+      if (item.inStock || item.orpakStock){
+        acc.push(
+          <tr key={index}>
+            <td className={classes.No}>{++index}</td>
+            <td>{item.sapcode}</td>
+            <td>{item.barcode}</td>
+            <td>{item.name}</td>
+            <td>{item.group}</td>
+            <td>{item.supplier}</td>
+            <td className={classes.Number}>{item.orpakStock}</td>
+            <td className={classes.Input}>
+              <div>
+                <input
+                  value={item.inStock}
+                  type="number"
+                  min="0"
+                  onChange={(e) => this.props.onChange(e, item.sapcode)}
+                />
+              </div>
+            </td>
+            <td className={classes.Number}>{difference}</td>
+            <Button onClick={() => this.popupMessage(item)}>Изтрий</Button>
+          </tr>
+        );
+      }
+      
+      return acc;
+      
+    }, []);
 
     const table = (
       <div className={classes.Inventory}>
